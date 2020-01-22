64cc19b2e322d16173b3dcd45ad4c9bb91757ade
mar-veloper <42553243+mar-veloper@users.noreply.github.com>
2020-01-22 18:09:44 +0100

    updated script.js

---
+++ Javascript-1/aw-21/js/script.js
@@ -0,0 +1,7 @@
+console.dir(document.querySelector("h1"));
+
+const heading2 = document.querySelector("h2");
+console.log(heading2);
+
+const headingByClass = document.querySelector(".heading2");
+console.log(headingByClass);
