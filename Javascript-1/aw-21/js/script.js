8d50926cb1fac3aede8706feb66f8571a1d7f1a8
mar-veloper <42553243+mar-veloper@users.noreply.github.com>
2020-01-23 09:27:59 +0100

    updated script.js

--- Javascript-1/aw-21/js/script.js
+++ Javascript-1/aw-21/js/script.js
@@ -5,9 +5,3 @@ console.log(heading2);
 
 const headingByClass = document.querySelector(".heading2");
 console.log(headingByClass);
-
-const qsLi = document.querySelector("li");
-console.log(qsLi);
-
-const qsaLi = document.querySelectorAll("li");
-console.log(qsaL1);
